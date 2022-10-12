c71c3aa063ab7606ba014b78faabd7d0e79ae791
leboaventura <leboaventura@deloitte.pt>
2022-10-12 12:42:00 +0100

    tests 3.2

--- src/test/java/com/aor/hero/model/arena/LoaderArenaBuilderTest.java
+++ src/test/java/com/aor/hero/model/arena/LoaderArenaBuilderTest.java
@@ -1,7 +1,6 @@
 package com.aor.hero.model.arena;
 
-import com.aor.hero.model.elements.Food;
-import com.aor.hero.model.elements.Wall;
+import com.aor.hero.model.elements.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -40,17 +39,53 @@ public class LoaderArenaBuilderTest {
     @Test
     void createFoods() throws IOException {
         LoaderArenaBuilder loaderArenaBuilder = Mockito.spy(new LoaderArenaBuilder(1));
-        List<Food> food = loaderArenaBuilder.createFoods();
-        Assertions.assertEquals(146, food.size());
+        List<Food> foods = loaderArenaBuilder.createFoods();
+        Assertions.assertEquals(146, foods.size());
 
-        Assertions.assertEquals(3, food.get(0).getPosition().getX());
-        Assertions.assertEquals(1, food.get(0).getPosition().getY());
-        Assertions.assertEquals(17, food.get(145).getPosition().getX());
-        Assertions.assertEquals(19, food.get(145).getPosition().getY());
+        Assertions.assertEquals(3, foods.get(0).getPosition().getX());
+        Assertions.assertEquals(1, foods.get(0).getPosition().getY());
+        Assertions.assertEquals(17, foods.get(145).getPosition().getX());
+        Assertions.assertEquals(19, foods.get(145).getPosition().getY());
+    }
 
+    @Test
+    void createPowers() throws IOException {
+        LoaderArenaBuilder loaderArenaBuilder = Mockito.spy(new LoaderArenaBuilder(1));
+        List<Power> powers = loaderArenaBuilder.createPowers();
+        Assertions.assertEquals(4, powers.size());
 
+        Assertions.assertEquals(2, powers.get(0).getPosition().getX());
+        Assertions.assertEquals(1, powers.get(0).getPosition().getY());
+        Assertions.assertEquals(18, powers.get(3).getPosition().getX());
+        Assertions.assertEquals(19, powers.get(3).getPosition().getY());
+    }
+
+    @Test
+    void createGhosts() throws IOException {
+        LoaderArenaBuilder loaderArenaBuilder = Mockito.spy(new LoaderArenaBuilder(1));
+        List<Ghost> ghosts = loaderArenaBuilder.createGhosts();
+        Assertions.assertEquals(4, ghosts.size());
+
+        Assertions.assertEquals(10, ghosts.get(0).getPosition().getX());
+        Assertions.assertEquals(7, ghosts.get(0).getPosition().getY());
+        Assertions.assertEquals(11, ghosts.get(3).getPosition().getX());
+        Assertions.assertEquals(9, ghosts.get(3).getPosition().getY());
+    }
+
+    @Test
+    void createPacman() throws IOException {
+        LoaderArenaBuilder loaderArenaBuilder = Mockito.spy(new LoaderArenaBuilder(1));
+        Pacman pacman = loaderArenaBuilder.createPacman();
+
+        Assertions.assertEquals(10, pacman.getPosition().getX());
+        Assertions.assertEquals(15, pacman.getPosition().getY());
 
     }
 
+    @Test
+    void getLines() throws IOException {
+        LoaderArenaBuilder loaderArenaBuilder = Mockito.spy(new LoaderArenaBuilder(1));
 
+        Assertions.assertNotNull(loaderArenaBuilder.getLines());
+    }
 }
