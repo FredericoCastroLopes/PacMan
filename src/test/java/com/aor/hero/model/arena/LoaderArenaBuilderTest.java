1a4d7909cade08574af79fff0e5b394cfed145d8
leboaventura <leboaventura@deloitte.pt>
2022-10-12 12:14:51 +0100

    tests 3.1.1

--- src/test/java/com/aor/hero/model/arena/LoaderArenaBuilderTest.java
+++ src/test/java/com/aor/hero/model/arena/LoaderArenaBuilderTest.java
@@ -1,5 +1,7 @@
 package com.aor.hero.model.arena;
 
+import com.aor.hero.model.elements.Food;
+import com.aor.hero.model.elements.Wall;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -23,6 +25,32 @@ public class LoaderArenaBuilderTest {
         Assertions.assertEquals(21, loaderArenaBuilder.getHeight());
     }
 
+    @Test
+    void createWalls() throws IOException {
+        LoaderArenaBuilder loaderArenaBuilder = Mockito.spy(new LoaderArenaBuilder(1));
+        List<Wall> walls = loaderArenaBuilder.createWalls();
+        Assertions.assertEquals(214, walls.size());
+
+        Assertions.assertEquals(1, walls.get(0).getPosition().getX());
+        Assertions.assertEquals(0, walls.get(0).getPosition().getY());
+        Assertions.assertEquals(19, walls.get(213).getPosition().getX());
+        Assertions.assertEquals(20, walls.get(213).getPosition().getY());
+    }
+
+    @Test
+    void createFoods() throws IOException {
+        LoaderArenaBuilder loaderArenaBuilder = Mockito.spy(new LoaderArenaBuilder(1));
+        List<Food> food = loaderArenaBuilder.createFoods();
+        Assertions.assertEquals(146, food.size());
+
+        Assertions.assertEquals(3, food.get(0).getPosition().getX());
+        Assertions.assertEquals(1, food.get(0).getPosition().getY());
+        Assertions.assertEquals(17, food.get(145).getPosition().getX());
+        Assertions.assertEquals(19, food.get(145).getPosition().getY());
+
+
+
+    }
 
 
 }
